4e002cc5d35969244783b636e3d5bb6746ed5fd6
aniklai <aron.niklai@stud.uni-corvinus.hu>
2022-11-27 12:11:17 +0100

    Webszolgáltatás hívása

--- Exchange/Exchange/Form1.cs
+++ Exchange/Exchange/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exchange.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Exchange
 {
@@ -15,6 +17,19 @@ namespace Exchange
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
