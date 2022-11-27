d1adb174059eeb0555580adea48ac0037f8c69a1
aniklai <aron.niklai@stud.uni-corvinus.hu>
2022-11-27 13:21:23 +0100

    chart added

--- Exchange/Exchange/Form1.cs
+++ Exchange/Exchange/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -70,6 +71,22 @@ namespace Exchange
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+        private void DataChart()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
     }
