231fe742df97f196cd43c0fdea69226af31eeef5
aniklai <aron.niklai@stud.uni-corvinus.hu>
2022-12-17 13:53:05 +0100

    Részvények felvéve

--- ValueAtRisk/ValueAtRisk/Form1.cs
+++ ValueAtRisk/ValueAtRisk/Form1.cs
@@ -27,7 +27,11 @@ namespace ValueAtRisk
 
         private void CreatePortfolio()
         {
-            
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
